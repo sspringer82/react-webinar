6ea028289ec6fdbf91a750363e2e900498826707
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-06-07 11:56:46 +0200

    error handling

--- 01-basics/src/List.tsx
+++ 01-basics/src/List.tsx
@@ -4,22 +4,38 @@ import ListItem from './ListItem';
 
 const List: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3001/books')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          setError('Das Laden der Bücher ist fehlgeschlagen');
+        }
+        return response.json();
+      })
       .then((booksFromServer) => setBooks(booksFromServer));
   }, []);
 
+  useEffect(() => {
+    if (error !== '') {
+      setTimeout(() => {
+        setError('');
+      }, 5000);
+    }
+  }, [error]);
+
   async function handleDelete(id: number): Promise<void> {
     if (confirm('biste sicher?')) {
-      const response = await fetch(`http://localhost:3001/books/${id}`, {
+      const response = await fetch(`http://localhost:3001/booksss/${id}`, {
         method: 'DELETE',
       });
       if (response.ok) {
         setBooks((previousBooks) => {
           return previousBooks.filter((book) => book.id !== id);
         });
+      } else {
+        setError('Das Löschen ist fehlgeschlagen');
       }
     }
   }
@@ -49,6 +65,7 @@ const List: React.FC = () => {
   return (
     <div>
       <h1>Bücherliste</h1>
+      {error != '' && <div>{error}</div>}
       {content}
     </div>
   );
